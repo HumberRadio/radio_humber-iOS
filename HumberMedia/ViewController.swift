4d39cdb72e4658488b98aa8f8ff31b3028a25022
Aleksandar Janakievski <aleksandarjanakievski@Aleksandars-MacBook-Air.local>
2019-01-25 13:50:25 -0500

    initial commit

---
+++ HumberMedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HumberMedia
+//
+//  Created by Aleksandar Janakievski on 1/25/19.
+//  Copyright © 2019 J-Fat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
